bb9b8a5636f07659173940cd8f0f160384ca47d3
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-05 17:35:36 +0530

    connected to DB

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 8001;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDb connection error", err);
+  });
