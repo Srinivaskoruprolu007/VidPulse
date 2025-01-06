5ec1c884d49125d8fd5a6b0e77f43f472ffba013
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-06 15:46:25 +0530

    add all models with schema

---
+++ src/models/subscription.models.js
@@ -0,0 +1,26 @@
+/*
+id string pk
+  subscriber ObjectId users
+  channel ObjectId users
+  createdAt Date
+  updateAt Date 
+ */
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
