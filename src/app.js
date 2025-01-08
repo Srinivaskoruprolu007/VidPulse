ff0d674777d65b262617ab6e2e6d561c82f86f7c
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-08 21:35:53 +0530

    update packges and util for ApiError

--- src/app.js
+++ src/app.js
@@ -1,25 +1,30 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(
-    cors({
-        origin:process.env.CORS_ORIGIN,
-        credentials:true
-    })
-)
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 // common middleware
-app.use(express.json({limit:"2mb"}));
-app.use(express.urlencoded({extended:true, limit:"2mb"}));
-app.use(express.static('public'));
+app.use(express.json({ limit: "2mb" }));
+app.use(express.urlencoded({ extended: true, limit: "2mb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // import routes
-import healthCheckRouter  from './routes/healthcheck.routes.js';
-
-
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 // routes
 
-app.use("/api/v1/healthCheck", healthCheckRouter)
+app.use("/api/v1/healthCheck", healthCheckRouter);
+app.use("/api/v1/users", userRouter);
 
-export {app};
+// error handler
+import { errorHandler } from "./middlewares/error.middlewares.js";
+app.use(errorHandler);
+export { app };
